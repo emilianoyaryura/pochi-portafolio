72c2311cc961350befc79b5582cb41133de5b3db
Emiliano Yaryura <emilianoyaryurat@gmail.com>
2021-04-07 22:45:02 -0300

    feat: portfolio contentful

---
+++ src/components/sections/portfolio/index.tsx
@@ -0,0 +1,7 @@
+import styles from './portfolio.module.css'
+
+const Portfolio = () => {
+  return <div className={styles.container}>hola</div>
+}
+
+export default Portfolio
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
 import PageLayout from 'components/layouts/page-layout'
-import HomeHero from 'components/sections/home-hero'
-import { getHero } from '../../lib/api'
+import HomeHero from 'components/sections/home-hero/home-hero'
+import Portfolio from 'components/sections/portfolio'
+import { getHero, getPortfolio } from '../../lib/api'
 
 type Props = {
   hero: string
+  portfolio: any
 }
 
-const HomePage = ({ hero }: Props) => {
+const HomePage = ({ hero, portfolio }: Props) => {
+  console.log(portfolio)
   return (
     <PageLayout withoutNav>
       <HomeHero hero={hero} />
+      <Portfolio />
     </PageLayout>
   )
 }
 
 export const getStaticProps = async () => {
   const hero = await getHero()
+  const portfolio = await getPortfolio()
 
   return {
     props: {
-      hero: hero[0].fields.heroTitle ?? null
+      hero: hero[0].fields.heroTitle ?? null,
+      portfolio: portfolio[0].fields.projects ?? null
     }
   }
 }
